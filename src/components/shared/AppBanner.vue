88a77bae5301012d6fe793bfbf040b4a29049a9d
MoElaSec <M.CS.Dev@protonmail.com>
2022-01-05 20:19:40 +0100

    My PDF + Intro

--- src/components/shared/AppBanner.vue
+++ src/components/shared/AppBanner.vue
@@ -8,17 +8,17 @@
 			<h1
 				class="text-3xl md:text-4xl xl:text-5xl text-center sm:text-left font-semibold text-ternary-dark dark:text-primary-light uppercase"
 			>
-				Hi, Iam Stoman
+				Hi, I'm Mo Eltahir
 			</h1>
 			<p
 				class="mt-4 text-xl md:text-2xl xl:text-4xl text-center sm:text-left font-semibold leading-none text-gray-400"
 			>
-				A Full-Stack Developer & Design Enthusiast
+				A Full-Stack Developer & Penetration Tester
 			</p>
 			<div class="flex justify-center sm:block">
 				<a
-					download="Stoman-Resume.pdf"
-					href="/files/Stoman-Resume.pdf"
+					download="Mo_ELtahir_Resume.pdf"
+					href="/files/Mo_ELtahir_Resume.pdf"
 					class="flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white"
 					aria-label="Download Resume"
 				>
