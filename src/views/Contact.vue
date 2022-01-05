466ab6b69f102afd560af18efc4a1db8b1aab573
MoElaSec <M.CS.Dev@protonmail.com>
2022-01-05 22:37:27 +0100

    📞Contact details

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -25,17 +25,17 @@ export default {
 			contacts: [
 				{
 					id: 1,
-					name: 'Your Address, Your City, Your Country',
+					name: 'Budapest, Hungary',
 					icon: 'map-pin',
 				},
 				{
 					id: 2,
-					name: 'email@domain.com',
+					name: 'mohd.debrecen@gmail.com',
 					icon: 'mail',
 				},
 				{
 					id: 3,
-					name: '555 8888 888',
+					name: 'nope..🙇‍♂️',
 					icon: 'phone',
 				},
 			],
